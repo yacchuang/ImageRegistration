9261e5a320fc723d73d841bdb00f7e66f51835b9
yacchuang <99505282+yacchuang@users.noreply.github.com>
2022-08-10 12:47:43 -0700

    stacked 3D cine to 4D images all time points

--- T1CineRegistration.py
+++ T1CineRegistration.py
@@ -1,3 +1,38 @@
-# Load T1 (masks) images and Cine images (all time points)
+import SimpleITK as sitk
+import nibabel as nib
+import numpy as np
+import os
 
-# Registration
+## Data path
+T1Address = '/Users/kurtlab/Desktop/Image_registration/ChiariSubj1/NIFTI/T1.nii'
+BrainStemAddress = '/Users/kurtlab/Desktop/Image_registration/ChiariSubj1/NIFTI/brainstemSsLabels.v12.FSvoxelSpace.nii'
+PCFAddress = '/Users/kurtlab/Desktop/Image_registration/ChiariSubj1/NIFTI/PCFExtractLabel.nii'
+CineAddress ="/Users/kurtlab/Desktop/Image_registration/ChiariSubj1/CineAllTimestep/"
+TimeSteps = 21
+
+## Load T1 and Cine images
+# T1 images: whole brain and PCF mask
+T1image = sitk.ReadImage(T1Address)
+BSimage = sitk.ReadImage(BrainStemAddress)
+PCFimage = sitk.ReadImage(PCFAddress)
+# Cine images (stacked all time points)
+Time = 0
+CineImageNP = nib.load(CineAddress+"WholeVolume_Time1.nii").get_fdata()
+CineImageShape = CineImageNP.shape
+
+
+## Registration
+# Transformation method:
+# Translation to Rigid (3D)
+# Rotation to Rigid (3D)
+# Rigid to similarity (3D)
+# Similarity to Affine
+# BSpline transformation
+# Displacement Field
+
+# Images and resampling:
+# set different output directory
+
+## Reading and Writing
+
+
